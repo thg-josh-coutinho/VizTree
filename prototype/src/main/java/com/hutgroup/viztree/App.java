293b885134a19114d72eee2d87b6f3598f6151fc
Joshua Coutinho <josh.coutinho@thehutgroup.com>
2016-12-06 18:07:32 +0000

    Added everything to intellij

--- prototype/src/main/java/com/hutgroup/viztree/App.java
+++ prototype/src/main/java/com/hutgroup/viztree/App.java
@@ -16,7 +16,7 @@ import java.util.stream.*;
 import java.util.function.*;
 import java.util.*;
 
-import java.io.StringReader; 
+import java.io.StringReader;
 
 import com.hutgroup.viztree.graph.FlowGraphListener;
 import org.jgrapht.event.FlowGraphEdgeChangeEvent;
@@ -30,57 +30,56 @@ import com.hutgroup.viztree.orderevents.*;
 import com.josh.utils.Tuple;
 import com.josh.utils.StringUtils;
 
-public class App
-{
+public class App {
 
-    FlowGraph graph;
-    Stream<List<FlowGraphEdgeChangeEvent>> edgeEventStream;
-    Unmarshaller jaxbUnmarshaller;
-    List<FlowGraphEdgeEvent> messages;
 
-    public App(Stream<List<FlowGraphEdgeChangeEvent>> inputStream, FlowGraph graph, Unmarshaller unmarshaller)
-    {
-	this.edgeEventStream = inputStream;
-	this.graph = graph;
-	this.unmarhsaller = unmarshaller;
+    public static final int EVENT_DELAY = 200;
+    private FlowGraph graph;
+    private Stream<List<FlowGraphEdgeChangeEvent>> edgeEventStream;
+    private Unmarshaller jaxbUnmarshaller;
+    private List<FlowGraphEdgeChangeEvent> messages;
 
-	initMessages();
+    public App(Stream<List<FlowGraphEdgeChangeEvent>> inputStream, FlowGraph graph, Unmarshaller unmarshaller) {
+        this.edgeEventStream = inputStream;
+        this.graph = graph;
+        this.jaxbUnmarshaller = unmarshaller;
     }
 
 
     /**
-     *
      * Updates the graph with each 'graphUpdate' pulled from the
      * MessageConsumer stream.
      */
     public void runApp() throws Exception // Remove this throws exception
     {
-	edgeEventStream.forEach((graphUpdate) -> {
-		     for(FlowGraphEdgeChangeEvent upd : graphUpdate) {
-			 update(graph, upd);
-		     }
-		});
+        edgeEventStream.forEach((graphUpdate) -> {
+            for (FlowGraphEdgeChangeEvent upd : graphUpdate) {
+                update(graph, upd);
+            }
+        });
     }
 
 
     /**
-     *
      * Updates the edge with the new weight recieved from the event.
      * Then waits to space out updates.
      */
-    private void update(FlowGraph g, FlowGraphEdgeChangeEvent update)
-    {
-	if(update == null) { System.err.println("Null update"); return; }
-	
-	g.setEdgeWeight(update.getEdge(), update.getNewWeight());
+    private void update(FlowGraph g, FlowGraphEdgeChangeEvent update) {
+        if (update == null) {
+            System.err.println("Null update");
+            return;
+        }
 
-	try {  Thread.sleep(EVENT_DELAY); }
-	catch(Exception e) {
-	    System.err.println("Err occurred when pausing within update inside App.java");
-	    e.printStackTrace();
-	}
+        g.setEdgeWeight(update.getEdge(), update.getNewWeight());
+
+        try {
+            Thread.sleep(EVENT_DELAY);
+        } catch (Exception e) {
+            System.err.println("Err occurred when pausing within update inside App.java");
+            e.printStackTrace();
+        }
 
     }
 
-   
+
 }
